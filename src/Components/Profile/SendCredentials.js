41ce768b8d030eda088e56cf3cf82a317614a4d9
StangaOne <evoke@stanga.net>
2022-07-07 12:46:06 +0300

    SendCredentials Component added

--- src/Components/Profile/SendCredentials.js
+++ src/Components/Profile/SendCredentials.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import styles from "./SendCredentials.module.scss";
 
 export const SendCredentials = () => {
-    return (
-        <div className={styles.sendCredentials}>
-            Send credentials
-        </div>
-    );
-}
+
+    const [userEmail, setUserEmail] = useState(null);
+
+  const inputEmailonChangeHandler = (e) => {
+    // console.log(e.target.value);
+    setUserEmail(e.target.value);
+    // TODO! : check if valid email and show error if not
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(userEmail);
+  }
+
+  return (
+    <div className={styles.sendCredentials}>
+      <div className={styles.formWrapper}>
+        <form>
+          <input
+            className="inputEmail"
+            type="email"
+            placeholder="email address"
+            onChange={inputEmailonChangeHandler}
+          />
+          <button className="submit" onClick={onSubmitHandler}>Send user &amp; pass to my email</button>
+        </form>
+      </div>
+      <p className={styles.info}>
+        Your email address will not be saved in our database. You will stay
+        completely anonymous. You will however have your login credentials in
+        your inbox.
+      </p>
+    </div>
+  );
+};
