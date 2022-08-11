98e1432745b1b8103bc55db41b98f90392ea8ff3
Georgi Karchev <georgi.karchev@gmail.com>
2022-08-11 17:24:35 +0300

    Timeline component added

---
+++ src/Components/Common/Timeline/Timeline.js
@@ -0,0 +1,29 @@
+import styles from "./Timeline.module.scss";
+
+import { stringToDdMonthYyyy } from "../../../Utils/dateUtils";
+
+export const Timeline = ({milestones}) => {
+
+    if(milestones && milestones.length > 0) {
+        console.log(milestones);
+    }
+
+    const line = milestones.map((m,i)=>{
+        // console.log(m)
+        return (
+            <li key={i} className={`${m.reached? styles.reached : ''} ${styles.milestone}`}>
+                <div className={styles.date}>{m.reached? stringToDdMonthYyyy(m.date) : 'today'}</div>
+                <div>{m.description}</div>
+            </li>
+        );
+    });
+    // const line = milestones.map
+
+    return (
+        <div className={styles.timelineWrapper}>
+            <ul className={styles.timeline}>
+                {line}
+            </ul>
+        </div>
+    );
+}
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,52 @@
+import { useContext, useEffect, useState } from "react";
+
+import { AuthContext } from "../../Contexts/AuthContext";
+
+import { Timeline } from "../Common/Timeline/Timeline";
+
 import styles from "./Dashboard.module.scss";
 
 export const Dashboard = () => {
+
+    const { profileData } = useContext(AuthContext);
+    const [ milestones, setMilestones ] = useState([])
+
+
+    useEffect(()=>{
+        if(profileData && profileData.permalink != undefined) {
+            console.log(profileData);
+            const newMilestones = [];
+            newMilestones.push({
+                date: profileData.created,
+                description: 'You Joined twok',
+                reached: true
+            });
+    
+            // ! TODO - fetch real achievements from server
+            newMilestones.push({
+                date: profileData.created,
+                description: 'You learned you first 25 words in German.',
+                reached: true
+            });
+            newMilestones.push({
+                date: profileData.created,
+                description: 'You completed the basics  basics with Eve.',
+                reached: true
+            });
+            newMilestones.push({
+                date: profileData.created,
+                description: 'Learn 25 more words.',
+                reached: false
+            });
+
+            setMilestones(newMilestones);
+        }
+    },[profileData]);
+
     return (
         <div className={styles.dashboard}>
             <h1>Dashboard</h1>
+            <Timeline milestones={milestones} />
         </div>
     );
 }
